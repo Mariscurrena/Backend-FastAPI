34eefad47491653710dfb2225f43ec0732d5007d
Mariscurrena <mariscurrenalae@gmail.com>
2025-08-19 10:57:11 -0600

    Added user/id endpoint

--- FastAPI/user.py
+++ FastAPI/user.py
@@ -5,15 +5,16 @@ app = FastAPI()
 
 ### With FastAPI and pydantic is possible create an Object and use it instead direct JSON
 class User(BaseModel): ###Base Model helps to create an entity without needing define constructors like in a conventional class
+    id: int
     name: str 
     surname: str
     url: str
     age: int ### Using Type Hints
 
 ### Imaginary users instead of connect the DB
-users_list = [User(name="Angel", surname="Mariscurrena", url="https://mariscurrena.github.io/Portfolio-AM/", age=24),
-              User(name="Andrea", surname="Trujillo", url="https://mariscurrena.github.io/Portfolio-AM/", age=24),
-              User(name="Emilio", surname="Mariscurrena", url="https://mariscurrena.github.io/Portfolio-AM/", age=12)] ### Creates instances of User class
+users_list = [User(id=1, name="Angel", surname="Mariscurrena", url="https://mariscurrena.github.io/Portfolio-AM/", age=24),
+              User(id=2, name="Andrea", surname="Trujillo", url="https://mariscurrena.github.io/Portfolio-AM/", age=24),
+              User(id=3, name="Emilio", surname="Mariscurrena", url="https://mariscurrena.github.io/Portfolio-AM/", age=12)] ### Creates instances of User class
 
 ###Example of how to work directly with JSON, not the best option in a OOP language
 @app.get("/usersjson")
@@ -43,3 +44,8 @@ async def usersjson():
 async def users():
     #return User(name="Angel", surname="Mariscurrena", url="https://mariscurrena.github.io/Portfolio-AM/", age=24) #Instance for an specific user using User's class
     return users_list
+
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list) #Filter = Superior order function
+    return list(users) ## Return a list with the filter function using lambda function results
