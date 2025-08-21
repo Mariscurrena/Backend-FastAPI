b706b7578e6c564c33eaa57d31f929420d240318
Mariscurrena <mariscurrenalae@gmail.com>
2025-08-21 17:27:03 -0600

    Fixed db_client reference

--- FastAPI/db/client.py
+++ FastAPI/db/client.py
@@ -1,3 +1,4 @@
 from pymongo import MongoClient
 
-db_client = MongoClient() ### If its not defined, it connects to the default mongo instance - localhost
+db_client = MongoClient().local ### If its not defined, it connects to the default mongo instance - localhost
+### Defined to local for this project
--- FastAPI/routers/users_db.py
+++ FastAPI/routers/users_db.py
@@ -13,7 +13,7 @@ users_list = []
 #-READ ALL-###########################################################################################################################
 @router.get("/all", response_model=list[User]) ## Specify that I want to return a list of users as an expected response
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 #-READ PATH-###########################################################################################################################
 @router.get("/{id}")
@@ -34,9 +34,9 @@ async def user(user: User):
     user_dict = dict(user) #As use pydantic, it's transformate to the needed MOdel (JSON)
     del user_dict["id"] # Errase ID due to it's automatic assign
 
-    id = db_client.local.users.insert_one(user_dict).inserted_id #Mongo instance - Schema, add inserted id
+    id = db_client.users.insert_one(user_dict).inserted_id #Mongo instance - Schema, add inserted id
 
-    new_user = user_schema(db_client.local.users.find_one({"_id":id})) # Looks for new user (id) in the DB, and returns a JSONS
+    new_user = user_schema(db_client.users.find_one({"_id":id})) # Looks for new user (id) in the DB, and returns a JSONS
     #ID in mongo is "_id
 
     return User(**new_user) ## Returns an user object
@@ -47,7 +47,7 @@ async def user(user: User):
     user_dict = dict(user) #Transform user input into a dictionary for being handle by mongo
     del user_dict["id"] #Delete ID because it can not change
     try:
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict) # Save user_dict, new parameter from user
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict) # Save user_dict, new parameter from user
     except:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User has not been updated")   
     
@@ -56,7 +56,7 @@ async def user(user: User):
 #-DELETE USER-########################################################################################################################### 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found: ## Mongo API returns deleted value when deleted successfully, so if not, found is empty and raise an error
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User has not been deleted")
@@ -65,7 +65,7 @@ async def user(id: str):
 # Search function that validates if the user already exists based on the given field type
 def search_user(field: str, key):  ## Performing a generic function to be able to reuse it
     try: 
-        user = user_schema(db_client.local.users.find_one({field: key})) # Perform transformation based on search criteria
+        user = user_schema(db_client.users.find_one({field: key})) # Perform transformation based on search criteria
         return User(**user) # Return user as a User instance of base model
     except:
         return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
